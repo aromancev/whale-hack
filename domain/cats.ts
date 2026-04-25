440cf50dee284cf2b91d70ad80203b0102fc9646
Andrey Romantsev <andrey@duna.com>
2026-04-25 16:02:35 +0200

    remove duplication in cat breeds

--- domain/cats.ts
+++ domain/cats.ts
@@ -79,52 +79,15 @@ export type CatBreed = {
 export type CatBreedForGroup<Group extends CatBreedGroup> =
     (typeof CAT_BREEDS_BY_GROUP)[Group][number]
 
-export const CAT_BREED_TO_GROUP = {
-    persian: 'flat_face_fluffy',
-    himalayan: 'flat_face_fluffy',
-    exotic_shorthair: 'flat_face_fluffy',
-    maine_coon: 'big_forest_cat',
-    norwegian_forest_cat: 'big_forest_cat',
-    siberian: 'big_forest_cat',
-    ragdoll: 'blue_eyes_point_fluffy',
-    birman: 'blue_eyes_point_fluffy',
-    balinese: 'blue_eyes_point_fluffy',
-    british_shorthair: 'round_teddy_cat',
-    british_longhair: 'round_teddy_cat',
-    scottish_fold: 'round_teddy_cat',
-    scottish_straight: 'round_teddy_cat',
-    chartreux: 'round_teddy_cat',
-    siamese: 'slim_big_ears',
-    oriental_shorthair: 'slim_big_ears',
-    oriental_longhair: 'slim_big_ears',
-    peterbald: 'slim_big_ears',
-    bengal: 'leopard_cat',
-    ocicat: 'leopard_cat',
-    egyptian_mau: 'leopard_cat',
-    savannah: 'leopard_cat',
-    toyger: 'leopard_cat',
-    cornish_rex: 'curly_hairless_cat',
-    devon_rex: 'curly_hairless_cat',
-    laperm: 'curly_hairless_cat',
-    sphynx: 'curly_hairless_cat',
-    donskoy: 'curly_hairless_cat',
-    abyssinian: 'athletic_fine_cat',
-    somali: 'athletic_fine_cat',
-    turkish_angora: 'athletic_fine_cat',
-    burmese: 'compact_sleek_cat',
-    bombay: 'compact_sleek_cat',
-    tonkinese: 'compact_sleek_cat',
-    havana_brown: 'compact_sleek_cat',
-    singapura: 'compact_sleek_cat',
-    japanese_bobtail: 'tail_different_cat',
-    kurilian_bobtail: 'tail_different_cat',
-    manx: 'tail_different_cat',
-    cymric: 'tail_different_cat',
-    european_shorthair: 'classic_house_cat',
-    american_shorthair: 'classic_house_cat',
-    domestic_shorthair: 'classic_house_cat',
-    domestic_longhair: 'classic_house_cat',
-} as const satisfies Record<CatBreed, CatBreedGroup>
+type CatBreedToGroup = {
+    [Group in CatBreedGroup as CatBreedForGroup<Group>]: Group
+}
+
+export const CAT_BREED_TO_GROUP = Object.fromEntries(
+    Object.entries(CAT_BREEDS_BY_GROUP).flatMap(([group, breeds]) =>
+        breeds.map((breed) => [breed, group])
+    ),
+) as CatBreedToGroup
 
 const catBreedGroups = Object.keys(CAT_BREEDS_BY_GROUP) as [CatBreedGroup, ...CatBreedGroup[]];
 const catBreeds = Object.values(CAT_BREEDS_BY_GROUP).flat() as [CatBreed, ...CatBreed[]];
